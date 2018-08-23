655d52b8f75fa5713f6f650eb0bf062718051bcf
Nunky Lado <puatachok@gmail.com>
2018-08-23 22:46:26 +0300

    Rollback

--- src/main/kotlin/DatabaseWorking.kt
+++ src/main/kotlin/DatabaseWorking.kt
@@ -12,10 +12,10 @@ class Person(map: Map<String, Any?>) {
 }
 
 class Example {
-    var user: List<Person> ?= null
+    private var user: MutableList<Person> ?= null
 
     fun start() {
-        user = listOf<Person>(
+        user = mutableListOf<Person>(
                 Person(mapOf("name" to "John Doe", "age" to 25)),
                 Person(mapOf("name" to "Peter Schulz", "age" to 45)),
                 Person(mapOf("name" to "Steven Kobs", "age" to 39)))
@@ -26,7 +26,7 @@ class Example {
         println(a)
         println("${a!!::class.qualifiedName}")
 
-        val db = DBMaker.fileDB("example.db").fileMmapEnable().make()
+        val db = DBMaker.fileDB("example.db").transactionEnable().fileMmapEnable().fileLockDisable().make()
 
         val map_name = db.hashMap<String, String>("example", Serializer.STRING, Serializer.STRING).createOrOpen()
         val map_age = db.hashMap<String, String>("example", Serializer.STRING, Serializer.STRING).createOrOpen()
@@ -39,8 +39,43 @@ class Example {
         db.close()
     }
 
+    fun add(name: String, age: Int) {
+        println(name + " " + age)
+        user?.add(Person(mapOf("name" to name, "age" to age)))
+        println("${user!!::class.qualifiedName}")
+
+        val db = DBMaker.fileDB("example.db").transactionEnable().fileMmapEnable().fileLockDisable().make()
+
+        val map_name = db.hashMap<String, String>("example", Serializer.STRING, Serializer.STRING).createOrOpen()
+        val map_age = db.hashMap<String, String>("example", Serializer.STRING, Serializer.STRING).createOrOpen()
+
+        map_name["name"] = user?.map { it.name }.toString()
+        map_age["age"] = user?.map { it.age }.toString()
+
+        println("${map_name.get("name")} ${map_age.get("age")}")
+        db.commit()
+        db.close()
+    }
+
+    fun rollback() {
+        val db = DBMaker.fileDB("example.db").transactionEnable().fileLockDisable().fileMmapEnable().make()
+
+        db.hashMap("example")
+        db.rollback()
+
+        val db_show = DBMaker.fileDB("example.db").closeOnJvmShutdown().transactionEnable().fileLockDisable().readOnly().make()
+        println("${db_show.hashMap("example").open().get("name")} ${db_show.hashMap("example").open().get("age")}")
+    }
+
+    fun show_with_add(name: String, age: Int) {
+        start()
+        add(name, age)
+        val db = DBMaker.fileDB("example.db").closeOnJvmShutdown().transactionEnable().fileLockDisable().readOnly().make()
+        println("${db.hashMap("example").open().get("name")} ${db.hashMap("example").open().get("age")}")
+    }
+
     fun show() {
-        val db = DBMaker.fileDB("example.db").closeOnJvmShutdown().readOnly().make()
+        val db = DBMaker.fileDB("example.db").closeOnJvmShutdown().transactionEnable().fileLockDisable().readOnly().make()
         println("${db.hashMap("example").open().get("name")} ${db.hashMap("example").open().get("age")}")
     }
 }
@@ -54,13 +89,22 @@ class StartConsole : CliktCommand() {
         } else if(readIinfo == "show") {
             TermUi.echo("Reading...")
             Example().show()
+        } else if(readIinfo == "a") {
+            Example().show_with_add("Albert Einstein", 100)
+        } else if(readIinfo == "rollback") {
+            Example().rollback()
+        } else if(readIinfo == "exit") {
+            System.exit(0)
         } else {
             println("Nothing to find")
+            System.exit(0)
         }
     }
 }
 
 fun main(args: Array<String>) {
-    TermUi.echo("Hello it is reading data")
-    StartConsole().main(args)
+    while (true) {
+        TermUi.echo("Hello it is reading data")
+        StartConsole().main(args)
+    }
 }
